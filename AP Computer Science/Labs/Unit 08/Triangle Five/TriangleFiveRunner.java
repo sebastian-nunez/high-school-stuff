9b9e008d69c31c5893bd96c92ca1455e43cd74ca
mikozera <02snunez@gmail.com>
2019-01-04 15:23:44 -0500

    missing unit 08 lab updated - triangle five

---
+++ AP Computer Science/Labs/Unit 08/Triangle Five/TriangleFiveRunner.java
@@ -0,0 +1,38 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Sebastian Nunez
+//Date - 12/12/18
+//Class - 10th
+//Lab  - Triangle 5
+
+import static java.lang.System.*;
+
+public class TriangleFiveRunner
+{
+   public static void main(String args[])
+   {
+      // Test cases
+      TriangleFive run = new TriangleFive('C', 4);
+      System.out.println(run);
+      System.out.println();
+
+      run.setLetter('A');
+      run.setAmount(5);
+      System.out.println(run);
+      System.out.println();
+
+      run.setLetter('B');
+      run.setAmount(7);
+      System.out.println(run);
+      System.out.println();
+
+      run.setLetter('X');
+      run.setAmount(6);
+      System.out.println(run);
+      System.out.println();
+
+      run.setLetter('Z');
+      run.setAmount(8);
+      System.out.println(run);
+      System.out.println();
+	}
+}
