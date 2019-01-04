3b0af2bc1be9c50ee583b6517f09b2094bf73480
mikozera <02snunez@gmail.com>
2019-01-04 15:16:22 -0500

    updated histogram lab

--- AP Computer Science/Labs/Unit 09/Histogram/Histogram.java
+++ AP Computer Science/Labs/Unit 09/Histogram/Histogram.java
@@ -14,7 +14,7 @@ public class Histogram
     private int numCount[];
     private int inputArray[];
 
-    //constructor
+    // constructor
     public Histogram() {
         numCount = new int[10];
     }
@@ -29,47 +29,19 @@ public class Histogram
     public String createHistogram() {
         String output = "";
 
+        // histogram creator
         for (int num: inputArray) {
-            switch (num) {
-                case 0:
-                    numCount[0] += 1;
-                    break;
-                case 1:
-                    numCount[1] += 1;
-                    break;
-                case 2:
-                    numCount[2] += 1;
-                    break;
-                case 3:
-                    numCount[3] += 1;
-                    break;
-                case 4:
-                    numCount[4] += 1;
-                    break;
-                case 5:
-                    numCount[5] += 1;
-                    break;
-                case 6:
-                    numCount[6] += 1;
-                    break;
-                case 7:
-                    numCount[7] += 1;
-                    break;
-                case 8:
-                    numCount[8] += 1;
-                    break;
-                case 9:
-                    numCount[9] += 1;
-                    break;
-                default:
-                    output += "ERROR: number must be between 0-9";
+            for (int i = 0; i < numCount.length; i++) {
+                if (num == i) {
+                    numCount[i]++;
+                }
             }
         }
 
         output += "Cool Histogram: \n";
 
-        // cool histogram
-        for (int i = 0; i <= 9; i++) {
+        // print cool histogram
+        for (int i = 0; i < numCount.length; i++) {
             output += String.format("%d - ", i);
             if (numCount[i] > 0) {
                 for (int j = 1; j <= numCount[i]; j++) {
@@ -81,8 +53,8 @@ public class Histogram
 
         output += "\n\nBoring Histogram: \n";
 
-        // boring histogram
-        for (int i = 0; i <= 9; i++) {
+        // print boring histogram
+        for (int i = 0; i < numCount.length; i++) {
             output += String.format("%d - %d\n", i, numCount[i]);
         }
 
