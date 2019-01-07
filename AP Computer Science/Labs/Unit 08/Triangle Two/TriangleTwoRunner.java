9e5b1320b2bf876952b7baa51fbe82eee3d1b50a
mikozera <02snunez@gmail.com>
2019-01-07 07:26:14 -0500

    triangle 2 fixed bug

--- AP Computer Science/Labs/Unit 08/Triangle Two/TriangleTwoRunner.java
+++ AP Computer Science/Labs/Unit 08/Triangle Two/TriangleTwoRunner.java
@@ -12,29 +12,25 @@ public class TriangleTwoRunner
 {
 
     public static void main(String args[]) {
-        try {
-            Scanner keyboard = new Scanner(System.in);
-            String choice = "";
+        Scanner keyboard = new Scanner(System.in);
+        String choice = "";
 
-            do {
-                out.print("Enter the size of the triangle : ");
-                int big = keyboard.nextInt();
+        do {
+            out.print("Enter the size of the triangle : ");
+            int big = keyboard.nextInt();
 
-                out.print("Enter a letter : ");
+            out.print("Enter a letter : ");
 
-                String value = keyboard.next();
+            String value = keyboard.next();
 
-                // instantiate a TriangleTwo object
-                TriangleTwo tw = new TriangleTwo(value, big);
+            // instantiate a TriangleTwo object
+            TriangleTwo tw = new TriangleTwo(value, big);
 
-                // call the toString method to print the triangle
-                System.out.println(tw);
+            // call the toString method to print the triangle
+            System.out.println(tw);
 
-                System.out.print("Do you want to enter more data? ");
-                choice = keyboard.next();
-            } while (choice.equals("Y") || choice.equals("y"));
-        } finally {
-            keyboard.close();
-        }
+            System.out.print("Do you want to enter more data? ");
+            choice = keyboard.next();
+        } while (choice.equals("Y") || choice.equals("y"));
     }
 }
