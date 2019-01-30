c5de28bf91ff52ff7de2b7db4ce009ffb0381dc1
mikozera <02snunez@gmail.com>
2019-01-30 00:53:25 -0500

    updated

---
+++ AP Computer Science/Labs/Unit 10/Number Sorter/NumberSorter.java
@@ -0,0 +1,40 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Sebastian Nunez
+//Date - 01/28/19
+//Class - 10th
+//Lab  - Number Sorter
+
+import java.util.Arrays; 
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import static java.lang.System.*;
+
+public class NumberSorter
+{
+	//instance variables and other methods not shown
+
+	private static int getNumDigits(int number)
+	{
+		int count = 0;
+    while (number > 0) {
+      count++;
+      number /= 10;
+    }
+		return count;
+	}
+
+	public static int[] getSortedDigitArray(int number)
+	{
+		int[] sorted = new int[getNumDigits(number)];
+    
+    for (int i = 0; i < sorted.length; i++) {
+      sorted[i] = number % 10;
+      number /= 10;
+    }
+    
+    Arrays.sort(sorted);
+ 
+		return sorted;
+	}
+}
---
+++ AP Computer Science/Labs/Unit 10/Number Sorter/NumberSorterRunner.java
@@ -0,0 +1,25 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Sebastian Nunez
+//Date - 01/28/19
+//Class - 10th
+//Lab  - Number Sorter
+
+import java.util.Arrays;
+import static java.lang.System.*; 
+
+public class NumberSorterRunner
+{
+	public static void main(String args[])
+	{
+		int[] cases = {567891, 901912468, 864213507, 898777, 234422};
+		for( int test : cases )
+		{
+			int[] one = NumberSorter.getSortedDigitArray( test );
+			for(int item : one)
+			{
+				System.out.print(item + " ");
+			}
+			System.out.println();
+		}
+	}
+}
