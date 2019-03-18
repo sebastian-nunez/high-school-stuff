f1296eeb085cadf318bfbfc85bf1f22b03618b5e
mikozera <02snunez@gmail.com>
2019-03-18 18:30:37 -0400

    Unit 15 Labs

---
+++ AP Computer Science/Labs/Unit 15/Number Analyzer/NumberAnalyzer.java
@@ -0,0 +1,44 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.ArrayList;
+
+public class NumberAnalyzer
+{
+    private ArrayList<Number> list;
+
+    public NumberAnalyzer() {
+
+    }
+
+    public NumberAnalyzer(String numbers) {
+
+    }
+
+    public void setList(String numbers) {
+
+    }
+
+    public int countOdds() {
+        int oddCount = 0;
+
+        return oddCount;
+    }
+
+    public int countEvens() {
+        int evenCount = 0;
+
+        return evenCount;
+    }
+
+    public int countPerfects() {
+        int perfectCount = 0;
+
+        return perfectCount;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
