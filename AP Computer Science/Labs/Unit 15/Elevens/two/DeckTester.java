6b2c99d91315409d44a799b859ea851e80fcd68d
mikozera <02snunez@gmail.com>
2019-03-22 21:23:56 -0400

    updated labs

---
+++ AP Computer Science/Labs/Unit 15/Elevens/two/DeckTester.java
@@ -0,0 +1,27 @@
+package two;
+
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester
+{
+
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        String[] ranks = {"jack", "queen", "king"};
+        String[] suits = {"blue", "red", "pink"};
+        int[] pointValues = {11, 12, 13};
+
+        Deck deck = new Deck(ranks, suits, pointValues);
+        System.out.println(deck);
+        deck.deal();
+        deck.deal();
+
+        System.out.println(deck);
+
+    }
+}
