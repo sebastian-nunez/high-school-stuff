9c9e724c14533aaa7e0bb0072523bf0e3ecbf41e
mikozera <02snunez@gmail.com>
2019-03-26 23:54:54 -0400

    updated projects

--- Projects/Languages/C/Basics/basics1.c
+++ Projects/Languages/C/Basics/basics1.c
@@ -1,70 +1,49 @@
-/* multi-line comment*/
-
-// single lien comment
-
-// imports functions such as printf() and scanf()
 #include <stdio.h>
-#include <string.h>
-
-// constant
-#define MYNAME "Sebastian"
-
-// global variable acessible to everysingle function in the program
-int globalVar = 100;
+#include <math.h>
 
 int main() {
-    // can use sizeOf() function to determine size of data type
+    int low, high, i, temp1, temp2, remainder, n = 0, result = 0;
 
-    // 1 byte (-128-127 or 0-255)
-    char firstLetter = 'S';
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &low, &high);
+    printf("Armstrong numbers between %d an %d are: ", low, high);
 
-    // 4 bytes (-2,147,483,648-2,147,483,647 or  0-4,294,967,295) could be 2 bytes
-    int age = 17;
+    for (i = low + 1; i < high; ++i) {
+        temp2 = i;
+        temp1 = i;
 
-    // 2 bytes (-32,768-32,767 or 0-65,535)
-    short altitude = 30000;
+        // number of digits calculation
+        while (temp1 != 0) {
+            temp1 /= 10;
+            ++n;
+        }
 
-    // 4 bytes (-2,147,483,648-2,147,483,647 or 0 to 4,294,967,295) there are also "long long"
-    long int superBigNum = 243434900;
+        // result contains sum of nth power of its digits
+        while (temp2 != 0) {
+            remainder = temp2 % 10;
+            result += pow(remainder, n);
+            temp2 /= 10;
+        }
 
-    // 4 bytes (1.2E-38 to 3.4E+38) single-point precision: 6 decimal places
-    float piValue = 3.14159;
+        // checks if number i is equal to the sum of nth power of its digits
+        if (result == i) {
+            printf("%d ", i);
+        }
 
-    // 8 bytes (2.3E-308 to 1.7E+308) double-point precision: 15 decimal places
-    double longerPi = 3.14159265359;
+        // resetting the values to check Armstrong number for next iteration
+        n = 0;
+        result = 0;
 
-    // 10 bytes (3.4E-4932 to 1.1E+4932) 19 decimal places
-    long double crazyNum = 39287432488234024.398247328947293;
-
-    /*--------------------------------------------------------------------------------------*/
-
-    // printing to terminal (escaped characters: \n = new line; \t = tab; \\ = \; \" = ")
-    printf("Hello, World!\n\n");
-
-    printf("I am %d years old\n\n", age);
-
-    printf("Big number %ld\n\n", superBigNum);
-
-    printf("Pi = %.5f\n\n", piValue);
-
-    printf("Long Pi = %.11f\n\n", longerPi);
-
-    printf("The first letter in my name is %c\n\n", firstLetter);
-
-    printf("My first name is %s\n\n", "Sebastian");
-
-    // creating strings(DON'T FORGET THE STRING TERMINATOR: \0 allocate one extra space)
-    char myFirstName[9] = "Sebastian";
-
-    char myLastName[] = "Nunez";
-
-    printf("My last name is %s\n\n", myLastName);
-
-    // from the string.h function you can use "strcpy()" to mutate a string
-    // be CAREFUL make sure the copied string is not larger than the original
-    strcpy(myLastName, "Torre");
-
-    printf("My new last name is %s\n\n", myLastName);
-
+    }
     return 0;
 }
+
+int convertBinaryToDecimal(long long n) {
+    int decimalNumber = 0, i = 0, remainder;
+    while (n != 0) {
+        remainder = n % 10;
+        n /= 10;
+        decimalNumber += remainder * pow(2, i);
+        ++i;
+    }
+    return decimalNumber;
