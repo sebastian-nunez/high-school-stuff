d2de938d629cfba0b81fd8063c7f4c2e19d68756
mikozera <02snunez@gmail.com>
2019-03-28 18:38:03 -0400

    updated react

---
+++ Projects/Languages/C/Basics/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_8c140.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_8c140.dir/feature_tests.c.obj"
+  "cmTC_8c140.pdb"
+  "cmTC_8c140.exe"
+  "cmTC_8c140.exe.manifest"
+  "libcmTC_8c140.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_8c140.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
