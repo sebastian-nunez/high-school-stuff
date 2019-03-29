2fdfea81259dce9af7cae8b7c1c3b9fa6fe52446
mikozera <02snunez@gmail.com>
2019-03-28 21:48:59 -0400

    updated react

--- Projects/React/React Guide/src/App.js
+++ Projects/React/React Guide/src/App.js
@@ -1,49 +1,43 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const app = () => {
-  const [personsState, setPersonsState] = useState({
-    persons: [
+const app = (props) => {
+  const [peopleState, setPeopleState] = useState({
+    people: [
       { name: "Sebastian", age: 17 },
       { name: "Max", age: 29 },
       { name: "Manu", age: 26 }
     ]
   });
 
+  const [otherState, setOtherState] = useState({
+    otherState: "some other state"
+  });
+
+  const switchNameHandler = () => {
+    setPeopleState({
+      people: [
+        { name: "Seb", age: 17 },
+        { name: "Max", age: 29 },
+        { name: "Manu", age: 26 }]
+    });
+  };
+
   return (
     <div className="App">
       <h1>React App</h1>
       <p>JSX</p>
 
-      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <button onClick={switchNameHandler}>Switch Name</button>
 
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      <Person name={peopleState.people[0].name} age={peopleState.people[0].age} />
+      <Person name={peopleState.people[1].name} age={peopleState.people[1].age}>My Hobbies: Racing</Person>
+      <Person name={peopleState.people[2].name} age={peopleState.people[2].age} />
     </div>
   );
 
-  // return React.createElement("div", {className: "App"}, React.createElement("h1", null, "Hi I'm a React App!"));
 };
 
 export default app;
-
-// state = {
-//   persons: [
-//     { name: "Sebastian", age: 17 },
-//     { name: "Max", age: 29 },
-//     { name: "Manu", age: 26 }
-//   ]
-// };
-//
-// switchNameHandler = () => {
-//   // console.log("Was clicked!");
-//   // DON'T: this.state.persons[0].name = "Seb";
-//   this.setState({
-//     persons: [
-//       { name: "Seb", age: 17 },
-//       { name: "Max", age: 29 },
-//       { name: "Manu", age: 26 }]
-//   });
-// };
