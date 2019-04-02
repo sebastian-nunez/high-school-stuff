4ba2f07fa8dc4646f66d74c6a4029298d0083ae7
mikozera <02snunez@gmail.com>
2019-04-02 16:06:34 -0400

    burger project updated

---
+++ Projects/React/React - The Complete Guide/burger-project/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import LayOut from "./components/Layout/Layout";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <LayOut />
+      </div>
+    );
+  }
+}
+
+export default App;
