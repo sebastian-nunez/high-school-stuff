5eb15ce48703ea1b26931fb78e771ac844b60607
mikozera <02snunez@gmail.com>
2019-04-07 22:47:45 -0400

    updated react course

--- Projects/React/React - The Complete Guide/routing--assignment-problem/src/containers/Course/Course.js
+++ Projects/React/React - The Complete Guide/routing--assignment-problem/src/containers/Course/Course.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
+  state = {
+    courseTitle: ""
+  };
+
+  componentDidMount() {
+    const courseTitle = this.parseQueryParam("courseTitle");
+    this.setState({ courseTitle });
+  }
+
+  componentDidUpdate() {
+    const courseTitle = this.parseQueryParam("courseTitle");
+    if (this.state.courseTitle !== courseTitle) {
+      this.setState({ courseTitle });
     }
+  }
+
+  parseQueryParam = (queryParam) => {
+    let parsedOutput = "";
+
+    const query = new URLSearchParams(this.props.location.search);
+    for (const param of query.entries()) {
+      parsedOutput = param[0] === queryParam ? param[1] : "";
+    }
+
+    return parsedOutput;
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.courseTitle ? this.state.courseTitle : null}</h1>
+        <p>You selected the Course with ID: {this.props.match.params.courseId}</p>
+      </div>
+    );
+  }
 }
 
 export default Course;
