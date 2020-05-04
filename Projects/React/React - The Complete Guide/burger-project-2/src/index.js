fc21ec10d1d01421f4f6215a3617101bd079f839
Sebastian Nunez <02snunez@gmail.com>
2020-05-04 19:52:06 -0400

    project structure update

--- Projects/React/React - The Complete Guide/burger-project-2/src/index.js
+++ Projects/React/React - The Complete Guide/burger-project-2/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./containers/App";
+import App from "./App";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 
